97867c71ba2a977efc4323bba1881355c034a33c
MartinCeball <121062706+MartinCeball@users.noreply.github.com>
2025-08-27 18:49:23 -0700

    Update Student.hpp

--- Student.hpp
+++ Student.hpp
@@ -4,32 +4,32 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
 
 
 class Student {
 private:
-    // Private member variables
+
     string* name;
     int* age;
 
 public:
-    // Constructor
+
     Student();
 
-    // Destructor
+
     ~Student();
 
-    // Setter for the student's name
+
     void setName(string s);
 
-    // Setter for the student's age
     void setAge(int i);
 
-    // Getter for the student's name 
+
     string getName() const;
 
-    // Getter for the student's age
+
     int getAge() const;
 };
 
